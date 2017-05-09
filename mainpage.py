85076889d78edce3f45c823d4874eae13c088058
zildjiean <piyapong-pp@hotmail.com>
2017-05-10 00:14:51 +0700

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -1,45 +1,37 @@
 from flask import Flask, request
 import json
 import requests
- 
 app = Flask(__name__)
- 
-@app.route('/')
-def index():
-    return "Hello World!"
-# ส่วน callback สำหรับ Webhook
-@app.route('/callback', methods=['POST'])
-def callback():
-    json_line = request.get_json()
-    json_line = json.dumps(json_line)
-    decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    #id=[d['replyToken'] for d in user][0]
-    #print(json_line)
-    print("ผู้ใช้：",user)
-    sendText(user,'งง') # ส่งข้อความ งง
-    return '',200
- 
+@app.route(‘/’)
+def index ():
+return “Hello World!”
+
+@app.route(‘/callback’, methods=[‘POST’])
+def callback ():
+json_line = request.get_json ()
+json_line = json.dumps(json_line)
+decoded = json.loads(json_line)
+user = decoded[“events”] [0] [‘replyToken’]
+print (“ผู้ใช้：”, user)
+sendText(user,’สวัสดี’)
+return ‘’,200
 def sendText(user, text):
-    LINE_API = 'https://api.line.me/v2/bot/message/reply'
-    Authorization = 'dlsm5320JnNyMnAzrscGOcGHImeaSw7uGzX2U9XGxdF3H15nLLx2rKo3gNcTaQEE9kZ0DEBD2q8qvraHNURcx8z/+8XRLsgnn85ZPPX22kc+NwsXs0B4eiRZxtGGvuCX+mIcsOXvL13WttwM7xjTSQdB04t89/1O/w1cDnyilFU=' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
- 
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization':Authorization
-    }
- 
-    data = json.dumps({
-        "replyToken":user,
-        "messages":[{
-            "type":"text",
-            "text":text
-        }]
-    })
- 
-    #print("ข้อมูล：",data)
-    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
-    #print(r.text)
- 
-if __name__ == '__main__':
-     app.run(debug=True)
+LINE_API = ‘https://api.line.me/v2/bot/message/reply'
+Authorization = ‘Bearer ENTER_ACCESS_TOKEN’ 
+# ใส่ ENTER_ACCESS_TOKEN เข้าไป
+headers = {
+‘Content-Type’: ‘application/json; charset=UTF-8’,
+‘Authorization’:Authorization
+}
+data = json.dumps({
+“replyToken”:user,
+“messages”: [{
+“type”:”text”,
+“text”:text
+}]
+})
+
+r = requests.post (LINE_API, headers=headers, data=data) # ส่งข้อมูล
+
+if __name__ == ‘__main__’:
+app.run(debug=True)
